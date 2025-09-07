34e3ae2763614014c14d2f883da0600d4ddec4e7
Affan-Codes <affan.khan4cs@gmail.com>
2025-09-07 16:28:09 +0530

    feat: implement user profile update functionality with validation and image upload support

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,7 +1,8 @@
 import type { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
-import { findByIdUserService } from "../services/user.service";
+import { findByIdUserService, updateUserService } from "../services/user.service";
 import { HttpStatus } from "../config/http.config";
+import { updateUserSchema } from "../validators/user.validator";
 
 export const getCurrentUserController = asyncHandler(
   async (req: Request, res: Response) => {
@@ -11,5 +12,19 @@ export const getCurrentUserController = asyncHandler(
     return res
       .status(HttpStatus.OK)
       .json({ message: "User fetched successfully", user });
+  }
+);
+
+export const updateUserController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const body = updateUserSchema.parse(req.body);
+    const userId = req.user?._id;
+    const profilePic = req.file;
+
+    const user = await updateUserService(userId, body, profilePic);
+
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: "User profile updated successfully", data: user });
   }
 );
